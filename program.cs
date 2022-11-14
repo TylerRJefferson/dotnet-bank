08bd3e166ecf87331d984ce294f9bab9f7bdce2b
Tyler Jefferson <tylerrjefferson00@gmail.com>
2022-11-14 14:32:40 -0500

    some examples of encapsulation, inheritance, methods

--- program.cs
+++ program.cs
@@ -1,1 +1,11 @@
-Console.WriteLine("Hello world");
+Console.WriteLine("Hello User");
+
+BankAccount account = new BankAccount(11000);
+//account.balance = 100.99m; // m needed after decimal numbers
+account.makeDeposit(100);
+
+//Console.WriteLine("You have " + account.balance + " dollars in your account");
+//Console.Write($"Your balance is: {account.balance} \n); // \n adds enter keystroke
+Console.WriteLine($"Your balance is: ${account.getBalance()}");
+Console.WriteLine($"Account Number: {account.accountNumber}");
+
